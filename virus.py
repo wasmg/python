3aec8923199c8d6ecde4ff23db774c17dc90a32d
saniya2008sl <90247192+saniya2008sl@users.noreply.github.com>
2021-09-11 17:24:46 +0530

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,10 @@
+import os
+for i in range(100):
+    os.system('start') ### this commd will be exectuted 100 times 
+### so it will open cmd 100 times
+
+              ##################         
+              ##[saniYa]########         
+              ##################
+              #########[virus]##
+              ################## 
